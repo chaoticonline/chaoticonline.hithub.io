3ad3c9509376fa64a132b77f03fba3ac2085c78f
Chaotic Online <dev@chaotic.online>
2017-10-30 13:00:01 -0200

    Site Update

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,53 @@
-(function(){if(navigator.userAgent.match(/IEMobile\/10\.0/)){var a=document.createElement("style");a.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}"));document.head.appendChild(a)}}());(function(c,b,a){c(function(){var e=b.location.hash;e&&c('ul.nav-pills a[href="'+e+'"]').tab("show");var d=c("#server-selection");if(e){d.css("display","none")}c(".nav-pills a").on("click",function(f){c(this).tab("show");d.hide();b.location.hash=this.hash;c("html, body").scrollTop(0)})})}(window.jQuery,window,document));
+/** NOT TO BE INCLUDED - Uglify and past inline at footer.html */
+
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2017 The Bootstrap Authors
+ * Copyright 2014-2017 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+// See the Getting Started docs for more information:
+// https://getbootstrap.com/getting-started/#support-ie10-width
+
+(function () {
+  'use strict'
+
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.head.appendChild(msViewportStyle)
+  }
+
+}());
+
+( function( $, window, document ) {
+
+    $( function() {
+
+        // Player Page Nav-Pills
+
+        var hash = window.location.hash;
+            hash && $( 'ul.nav-pills a[href="' + hash + '"]' ).tab( 'show' );
+
+        var $serverSelection = $( '#server-selection' );
+
+        if( hash ) $serverSelection.css( 'display', 'none' );
+
+        $( '.nav-pills a' ).on( 'click', function( e ) {
+
+          $( this ).tab( 'show' );
+
+          $serverSelection.hide();
+
+          window.location.hash = this.hash;
+
+          $( 'html, body' ).scrollTop( 0 );
+        });
+    });
+
+}( window.jQuery, window, document ) );
